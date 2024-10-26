33abf383da12dd720273602d81fbc70a65b8b55f
Per Ørholm Nielsen <per@qrholm.dk>
2024-10-27 01:36:36 +0200

    fix

--- helth-observations/work.py
+++ helth-observations/work.py
@@ -17,15 +17,25 @@ pprint.pprint(data)
 
 
 diastolic_series = []
+systolic_series =[]
+heart_rate_series = []
 
 for year, dates in data.items():
     for date_str, times in dates.items():
         for time_str, details in times.items():
-            if details.get('type') == 'blood-pressure' and 'diastolic' in details:
-
+            if details.get('type') == 'blood-pressure':
                 datetime_str = f"{year}-{date_str} {time_str}"
                 timestamp = datetime.strptime(datetime_str, "%Y-%d-%m %H:%M")
-                diastolic_value = details['diastolic']
-                diastolic_series.append((timestamp, diastolic_value))
+                if 'diastolic' in details:
+                    diastolic_value = details['diastolic']
+                    diastolic_series.append((timestamp, diastolic_value))
+                if 'systolic' in details:
+                    systolic_value = details['systolic']
+                    systolic_series.append((timestamp, systolic_value))
+                if 'heart_rate' in details:
+                    heart_rate_value = details['heart_rate']
+                    heart_rate_series.append((timestamp, heart_rate_value))
 
 plot = Plot(diastolic_series, 'diastolic pressure', x_type='time', y_type='mmHg', destination=plot_root)
+plot = Plot(systolic_series, 'systolic pressure', x_type='time', y_type='mmHg', destination=plot_root)
+plot = Plot(heart_rate_series, 'heart rate', x_type='time', y_type='/min', destination=plot_root)
